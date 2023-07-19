7c24e12047f4e28c2136ec10be32f01f9830e486
fs1g17 <fs1g17@soton.ac.uk>
2023-07-19 15:52:01 +0100

    feat(mui): add MUI

---
+++ src/config/theme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#512B7C",
+    },
+    primary: {
+      light: "#744FA4",
+      main: "#512B7C",
+      dark: "#320162",
+    },
+    secondary: {
+      light: "#EC407A",
+      main: "#E91E63",
+      dark: "#C2185B",
+    },
+  },
+});
+export default theme;
