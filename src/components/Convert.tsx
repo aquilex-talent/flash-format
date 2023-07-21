bbb2df6d04615268f2e84f2a5c370659859b4f98
fs1g17 <fs1g17@soton.ac.uk>
2023-07-21 19:21:16 +0100

    poop

--- src/components/Convert.tsx
+++ src/components/Convert.tsx
@@ -1,9 +1,50 @@
 import { Box, Button, Typography, useTheme } from "@mui/material";
-import React from "react";
+import React, { useRef, useState } from "react";
 import ConvertOptions from "./ConvertOptions";
+import axios from "axios";
 
 const Convert = () => {
   const theme = useTheme();
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  console.log({ selectedFile })
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.item(0);
+    if (file) {
+      console.log("here")
+      poop(file);
+    }
+  };
+
+  const onFileUpload = () => {
+    if (!selectedFile) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append(selectedFile.name, selectedFile);
+
+    axios.post(
+      "http://127.0.0.1:5001/flash-format/us-central1/convertImage",
+      formData
+    );
+  };
+
+  const poop = async (f: File) => {
+    const formData = new FormData();
+    formData.append(f.name, f);
+
+    try {
+      console.log("sending file")
+      await axios.post(
+        "http://127.0.0.1:5001/flash-format/us-central1/convertImage",
+        formData
+      );
+      console.log("SUCCESS");
+    } catch(error) {
+      console.log('error: ', error);
+    }
+  }
   return (
     <Box
       display="flex"
@@ -16,16 +57,27 @@ const Convert = () => {
       }}
     >
       <ConvertOptions />
+      <input
+        type="file"
+        ref={inputRef}
+        onChange={onFileChange}
+        style={{ display: "none" }}
+      />
       <Button
+        onClick={() => {
+          if (inputRef.current) {
+            inputRef.current.click();
+          }
+        }}
         sx={{
           width: "fill-parent",
           backgroundColor: theme.palette.secondary.main,
           color: "white",
           textTransform: "none",
-          marginTop: '50px',
-          '&:hover': {
-            backgroundColor: theme.palette.secondary.dark
-          }
+          marginTop: "50px",
+          "&:hover": {
+            backgroundColor: theme.palette.secondary.dark,
+          },
         }}
       >
         <Typography variant="body1" color="white">
